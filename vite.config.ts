bebda969a3e26d7fc043ba9a7a95b22e02fbd4a9
orestsharak <sharak.orest@gmail.com>
2025-09-26 11:15:36 +0200

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -15,16 +14,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    typecheck: {
-      tsconfig: './tsconfig.test.json',
-    },
-    include: ['src/**/*.test.tsx'],
-  },
-  define: {
-    'process.env': process.env,
-  },
 })
